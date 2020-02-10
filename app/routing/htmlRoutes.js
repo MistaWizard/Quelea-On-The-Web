f8491e648e81981ec9bcbc88ff9df256771a2b9e
MistaWizard <tylermckellar@gmail.com>
2020-02-09 23:58:42 -0500

    Cleaning routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = function(app) {
-    // app.get("/survey", function(req, res) {
-    //     res.sendFile(path.join(__dirname + "/../public/survey.html"));
-    // });
+    app.get("/lower", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/lowerthirds.html"));
+    });
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/index.html"));
     });
