c11d4d0a60cb58d2d627ab5d66ea7a88e8a49432
MistaWizard <tylermckellar@gmail.com>
2020-09-28 02:13:25 -0400

    Backend API Polling and SSE to frontend Lyrics

--- app/public/assets/javascript/lyrics.js
+++ app/public/assets/javascript/lyrics.js
@@ -1,13 +1,13 @@
 // Create EventSource for the SSE from the Backend
-const evtSource = new EventSource("http://localhost:3000/lowerthirdsserver");
+const evtSource = new EventSource('/lowerthirdsserver');
 
 let lyricsText = "";
 
 // Main function that repeats ever 500ms
 theWhole = () => {
-    setInterval(() => {
-        runIt();
-    }, 500);
+    // setInterval(() => {
+    //     runIt();
+    // }, 500);
 };
 
 // // Open the EventListener for the SSE stream
@@ -18,45 +18,67 @@ theWhole = () => {
 // // How we are going to handle SSE messages from the backend
 // evtSource.addEventListener('message', (e) => {
 //     console.log('Received a message:', e.data);
+//     const messageData = e.data;
+
+//     console.log(messageData);
+//         // Checking if our latest data is the same as what we already have
+//         if (lyricsText === messageData) {
+//             console.log("No change is good change");
+//         }
+//         // Proceed if data is different than what is currently set
+//         else {
+//             // FadeOut if the newest data is empty
+//             if (messageData === "") {
+//                 $("#liveLyrics").fadeOut(300, () => {
+//                     $(this).empty().show();
+//                 });
+//                 lyricsText = messageData;
+//             }
+//             // Proceed if newest data isn't empty
+//             else {
+//                 // Update and FadeIn if #liveLyrics div is empty
+//                 if (lyricsText === "") {
+//                     lyricsText = messageData;
+//                     $("#liveLyrics").hide();
+//                     $("#liveLyrics").html(lyricsText);
+//                     $("#liveLyrics").fadeIn(300, () => {
+//                         console.log(this);
+//                     });
+//                 }
+//                 // Update #liveLyrics div with newest data
+//                 else {
+//                     lyricsText = messageData;
+//                     console.log("Here are the lyrics:" + lyricsText);
+//                     $("#liveLyrics").html(lyricsText);
+//                 }
+//             }
+//         }
+
 // }, false);
 
 // Listen for SSE messages and format them
 evtSource.onmessage = (e) => {
     const messageData = e.data;
 
-    console.log("Here be the data: " + messageData);
-};
-
-// Function to connect to Quelea Lyrics API
-runIt = () => {
-    const queryURL = "lyricsApi/lyrics";
-
-    // Calling Ajax
-    $.ajax({
-        url: queryURL, 
-        method: "GET",
-
-    }).done((data) => {
-        console.log(data);
-
+    console.log(messageData);
         // Checking if our latest data is the same as what we already have
-        if (lyricsText === data) {
+        if (lyricsText === messageData) {
             console.log("No change is good change");
         }
         // Proceed if data is different than what is currently set
         else {
             // FadeOut if the newest data is empty
-            if (data === "") {
+            if (messageData === "") {
                 $("#liveLyrics").fadeOut(300, () => {
                     $(this).empty().show();
                 });
-                lyricsText = data;
+                lyricsText = messageData;
             }
             // Proceed if newest data isn't empty
             else {
                 // Update and FadeIn if #liveLyrics div is empty
                 if (lyricsText === "") {
-                    lyricsText = data;
+                    lyricsText = messageData;
                     $("#liveLyrics").hide();
                     $("#liveLyrics").html(lyricsText);
                     $("#liveLyrics").fadeIn(300, () => {
@@ -65,18 +87,65 @@ runIt = () => {
                 }
                 // Update #liveLyrics div with newest data
                 else {
-                    lyricsText = data;
+                    lyricsText = messageData;
                     console.log("Here are the lyrics:" + lyricsText);
                     $("#liveLyrics").html(lyricsText);
                 }
             }
         }
 
-    // Catch any errors that occur
-    }).catch((error) => {
-        console.log(error);
-    });
 };
+
+// // Function to connect to Quelea Lyrics API
+// runIt = () => {
+//     const queryURL = "lyricsApi/lyrics";
+
+//     // Calling Ajax
+//     $.ajax({
+//         url: queryURL, 
+//         method: "GET",
+
+//     }).done((data) => {
+//         console.log(data);
+
+//         // Checking if our latest data is the same as what we already have
+//         if (lyricsText === data) {
+//             console.log("No change is good change");
+//         }
+//         // Proceed if data is different than what is currently set
+//         else {
+//             // FadeOut if the newest data is empty
+//             if (data === "") {
+//                 $("#liveLyrics").fadeOut(300, () => {
+//                     $(this).empty().show();
+//                 });
+//                 lyricsText = data;
+//             }
+//             // Proceed if newest data isn't empty
+//             else {
+//                 // Update and FadeIn if #liveLyrics div is empty
+//                 if (lyricsText === "") {
+//                     lyricsText = data;
+//                     $("#liveLyrics").hide();
+//                     $("#liveLyrics").html(lyricsText);
+//                     $("#liveLyrics").fadeIn(300, () => {
+//                         console.log(this);
+//                     });
+//                 }
+//                 // Update #liveLyrics div with newest data
+//                 else {
+//                     lyricsText = data;
+//                     console.log("Here are the lyrics:" + lyricsText);
+//                     $("#liveLyrics").html(lyricsText);
+//                 }
+//             }
+//         }
+
+//     // Catch any errors that occur
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+// };
 
 // Run the whole process
 theWhole();
--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const axios = require("axios");
 const path = require("path");
 const methodOverride = require("method-override");
 const config = require("./config.js");
+const rcUrl = config.rcApi + "/lyrics";
+const lyricUrl = config.lyricsApi + "/lyrics";
 
-let lowerLyrics = ""
+let lowerLyrics = "";
+let newLyrics = "";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -34,8 +37,6 @@ app.all("/lyricsApi/*", (req, res) => {
 });
 
 // Quelea RC API GET using Axios
-const rcUrl = "http://localhost:1112/lyrics";
-
 axios.get(rcUrl).then(response => {
     console.log("Here be the RC Lyrics " + response.data);
 })
@@ -43,16 +44,36 @@ axios.get(rcUrl).then(response => {
     console.log(error);
 });
 
-const lyricUrl = "http://localhost:1111/lyrics";
 
 // Quelea Lyrics API GET using Axios
-axios.get(lyricUrl).then(res => {
-    console.log("Here be the Lyrics " + res.data);
-    // res = lowerLyrics;
-})
-.catch(error => {
-    console.log(error);
-});
+// axios.get(lyricUrl).then(res => {
+//     console.log("Here be the Lyrics " + res.data);
+//     console.log(lowerLyrics);
+// })
+// .catch(error => {
+//     console.log(error);
+// });
+
+setInterval(() => {
+
+    // Quelea Lyrics API GET using Axios
+    axios.get(lyricUrl).then(res => {
+        // console.log("Here be the Lyrics " + res.data);
+        newLyrics = res.data;
+        // console.log(newLyrics);
+        if (newLyrics === lowerLyrics) {
+            console.log("No change is good change");
+        }
+        else {
+            lowerLyrics = newLyrics;
+            console.log("Here it is");
+            // response.write('data:' + lowerLyrics + '\n\n')
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}, 300);
 
 // SSE Connection to the Lower Thirds Lyric Front End
 app.get('/lowerthirdsserver', function (request, response) {
@@ -60,13 +81,18 @@ app.get('/lowerthirdsserver', function (request, response) {
         "connection": "keep-alive",
         "cache-control": "no-cache",
         "content-type": "text/event-stream",
-    })
-    const data = "Hello Earl!";
-    setInterval(() => {
-        response.write('data: Hello Steve!!' + data + '\n\n')
-    }, 500)
-});
+    });
+    // const data = "Hello Earl!";
 
+    setInterval(() => {
+        response.write('data:' + lowerLyrics + '\n\n');
+    }, 300);
+
+    // request.on('close', () => {    
+    //     response.end();    
+    //     console.log('Stopped sending events.');  
+    // });
+});
 
 // Keep server running if connection to an API fails
 process.on('uncaughtException', (err) => {
